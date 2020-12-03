181cdb940f3af633cf16e0098acd4e6db265e437
annyeong1 <55046023+annyeong11@users.noreply.github.com>
2020-12-04 02:19:12 +0300

    Add files via upload

---
+++ js/main2.js
@@ -0,0 +1,37 @@
+// var a = 35;
+// var b = 'Hello';
+// a = prompt ('Enter a');
+
+// if (a==37) {
+//     alert ('a = 37')
+// } else {
+//     alert ('emm')
+// };
+
+// for (var i=0; i<5; i++) {
+//     alert (i)
+// }
+
+
+// var user = {
+//     'name': 'Andrew',
+//     'surname': 'Stones',
+//     'age': 25
+// }
+
+// alert (user.name)
+
+// var mas = ['Andrew', 'Stones', 25];
+
+// alert (mas[0]) 
+
+
+// function doMagic (a,base) {
+//     alert (a + base);
+// }
+
+// doMagic(10,5);
+
+
+
+
